bf257785682cff26803ddc98da5779667f2ddc89
germanlantsman <germanlantsman@gmail.com>
2023-01-12 23:57:21 +0300

    feat: update index.js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
   
+  
+  if (n > 0) {
+
+    return n.toString().split('').reverse().join('');
+  }
+  else {
+    let m = Math.abs(n)
+    return m.toString().split('').reverse().join('');
+  }
+  
 }
